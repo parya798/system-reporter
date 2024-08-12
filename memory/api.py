5fb9cc4b2bb4ade59dcdc67b24668b41f557a033
paryaM <parya797@yahoo.com>
2024-08-12 04:44:56 -0400

    .default_number

--- memory/api.py
+++ memory/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, g 
+from flask import Flask, g 
 from memory.services import RamStatsService 
 import sqlite3
 
@@ -16,9 +16,10 @@ def close_connection(exception):
     db = getattr(g, '_database', None) 
     if db is not None: 
         db.close() 
-    
+
+DEFAULT_NUM_RECORDS = 5
+
 @app.route("/last_ram_stats") 
 def get_last_ram_stats_api(): 
-    num_records = int(request.args.get("num_records", 10)) 
     cursor = get_db().cursor() 
-    return RamStatsService.get_last_ram_stats(cursor, num_records)
+    return RamStatsService.get_last_ram_stats(cursor, DEFAULT_NUM_RECORDS)
